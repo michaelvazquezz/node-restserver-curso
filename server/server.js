5af3e4ab5bd1306ff96bf6e487e73d740c97c35b
Your Name <you@example.com>
2020-11-01 15:32:41 -0300

    RestServer -usuario

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,46 +11,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
 
 
-app.get('/usuario', function(req, res) {
 
-    res.json('get usuario');
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
+mongoose.connect(process.env.URLDB,
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-    res.json({
-        body
+    { useNewUrlParser: true, useCreateIndex: true },
+
+    () => {
+
+        // if (err) throw err;
+        console.log('Base de datos online');
     });
-})
-
-
-app.put('/usuario:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-})
-
-
-app.delete('/usuario', function(req, res) {
-
-    res.json('Hola mi gente');
-})
-
-
-
 
 
 app.listen(process.env.PORT, () => {
